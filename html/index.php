38a4724b07694e9e50c4fb520fa667cfdb527f4a
Siddhikapatil <Siddhikampatil30@gmail.com>
2022-05-05 11:43:28 +0530

    init

---
+++ html/index.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>ePaper</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+
+<body style="overflow: initial;">
+    <div class="full-body">
+        <nav class="first-navbar language">
+            <div class="first-div">
+                <ul>
+                    <li class="item click-event">
+                        <a href="# ">English
+                            <strong>English</strong>
+                        </a>
+
+                    </li>
+                    <li class="item click-event">
+                        <a href="# ">Hindi
+                            <strong>Hindi</strong>
+                        </a>
+
+                    </li>
+                    <li class="item click-event">
+                        <a href="# ">Marathi
+                            <strong>Marathi</strong>
+                        </a>
+
+                    </li>
+                    <li class="item click-event">
+                        <a href="# ">தமிழ்
+                            <strong>Tamil</strong>
+                        </a>
+
+                    </li>
+                    <li class="item click-event">
+                        <a href="# ">বাংলা
+                            <strong>Bangla</strong>
+                        </a>
+
+                    </li>
+                    <li class="item click-event">
+                        <a href="# ">മലയാളം
+                            <strong>Malyalam</strong>
+                        </a>
+
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <nav class="first-navbar Filter first-div">
+            <div class="m-div">
+                <ul>
+                    <li class="item"><a href="#home">Home</a></li>
+                    <li class="item"><a href="#State&City">State&City</a></li>
+                    <li class="item"><a href="#World">World</a></li>
+                    <li class="item"><a href="#Sports">Sports</a></li>
+                    <li class="item"><a href="#Entertainment">Entertainment</a></li>
+                    <li class="item"><a href="#Lifestyle">Lifestyle</a></li>
+                    <li class="item"><a href="#Live">Live</a></li>
+                    <li class="item"><a href="#publishing">publishing</a></li>
+                    <li class="item"><a href="#EPaper">EPaper</a></li>
+
+                </ul>
+            </div>
+            <div class="l-div f-wrapper">
+                <ul>
+                    <li>
+                        <a href="../php/login.html">
+                            <button class='loginbtn' onclick="document.getElementById('login-form').style.display='block'" style="width:auto;">Login</button>
+                        </a>
+
+                    </li>
+
+                </ul>
+            </div>
+
+        </nav>
+
+    </div>
+    </div>
+    <span>
+        <nav class="first-navbar H-title">
+            <!-- <div id="logo">
+                    <img src="../img/logo.jpg" alt="MyIndia.com">
+                </div> -->
+            <div>
+                <center>
+                    <h1>The Daily Hub</h1>
+                </center>
+            </div>
+            <div class="f-wrapper follow">
+
+
+                <div class="follow-icon">
+                    <!-- <h3>Follow us on :</h3> -->
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-linkedin"></a>
+
+                </div>
+            </div>
+        </nav>
+    </span>
+    <div class="T-date">
+        <span class="date-update">Tue,8 Feb</span>
+    </div>
+
+
+    <section id="content-wrapper">
+
+        <aside class="leftnews">
+            <div class="Lfnews">
+                <h2 class="fheading">
+                    <span class="hd-lines">
+                    </span>
+                    <span class="head-style">
+                        <a href="#Latest-news">
+                            <span>[</span> Latest News
+                    <span>]</span>
+                    </a>
+
+                    </span>
+                </h2>
+                <div class="Lnews-content">
+                    <div class="row-news">
+                        <h3 class="first-Nheading">
+                            <a href="#news">"India Recorded Second-Highest Cryptocurrency Users In World In 2021: Report
+                                "</a>
+                        </h3>
+                        <div class="date-update">publish on : 10 Feb 2022, 10:10 Am </div>
+                    </div>
+                    <div class="row-news">
+                        <h3 class="first-Nheading">
+                            <a href="news">"New York drops mask mandate as it bounces back from Covid-19 pandemic
+                                sabbatical"</a>
+                        </h3>
+                        <div class="date-update">publish on : 10 Feb 2022, 10:10 Am </div>
+                    </div>
+                    <div class="row-news">
+                        <h3 class="first-Nheading">
+                            <a href="news">"India's historic win at gabba: India won by 3 wickets at gabba."</a>
+                        </h3>
+                        <div class="date-update">publish on : 10 Feb 2022, 10:10 Am </div>
+                    </div>
+                    <div class="row-news">
+                        <h3 class="first-Nheading">
+                            <a href="news">"RBI keeps repo rate unchanged at 4%; maintains accommodative stance"</a>
+                        </h3>
+                        <div class="date-update">publish on : 10 Feb 2022, 10:10 Am </div>
+                    </div>
+                    <div class="row-news">
+                        <h3 class="first-Nheading">
+                            <a href="news">"Global Covid-19 case counts decline 17% in latest week:World Health
+                                Organisation(WHO)"</a>
+                        </h3>
+                        <div class="date-update">publish on : 10 Feb 2022, 10:10 Am </div>
+                    </div>
+
+                </div>
+            </div>
+        </aside>
+        <section class="main-container content-wrapper">
+            <div>
+
+            </div>
+            <section class="main-content">
+                <h2 class="fheading">
+                    <span class="hd-lines">
+                    </span>
+                    <span class="head-style">
+                        <a href="#Latest-news">
+                            <span>[</span> Today's Paper
+                    <span>]</span>
+                    </a>
+                    </span>
+                </h2>
+                <div class="Lnews-content">
+                    <div class="content-holder">
+                        <div class="row-news">
+                            <h3 class="first-Nheading">
+                                <a href="#news">"India's historic win at gabba: India won by 3 wickets at gabba."
+                                    "</a>
+                            </h3>
+                        </div>
+
+                        <figure>
+                            <span>
+                                <a href="#gabba">
+                                    <img src="../img/gaba_win.png" alt="">
+                                </a>
+                            </span>
+                        </figure>
+                    </div>
+                </div>
+
+            </section>
+        </section>
+
+        <aside class="radvertise">
+            <div class="right-div">
+                <div class="ad-head">
+                    <div class="r-content advertise">
+                        <a href="#">
+                            <img src="../img/advertise-1.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="leftnews Lfnews t-news">
+                <div class="br-div">
+                    <h2 class="fheading">
+                        <span class="hd-lines">
+                        </span>
+                        <span class="head-style">
+                            <a href="#Latest-news">
+                                <span>[</span> For You
+                        <span>]</span>
+                        </a>
+
+                        </span>
+                    </h2>
+                    <div class="Lnews-content">
+                        <div class="row-news">
+                            <h3 class="first-Nheading">
+                                <a href="#news">"India Recorded Second-Highest Cryptocurrency Users In World In 2021:
+                                    Report
+                                    "</a>
+                            </h3>
+                            <div class="date-update">publish on : 10 Feb 2022, 10:10 Am </div>
+                        </div>
+
+                    </div>
+                    <div class="row-news">
+                        <h3 class="first-Nheading">
+                            <a href="news">"New York drops mask mandate as it bounces back from Covid-19 pandemic
+                                sabbatical"</a>
+                        </h3>
+                        <div class="date-update">publish on : 10 Feb 2022, 10:10 Am </div>
+                    </div>
+                    <div class="row-news">
+                        <h3 class="first-Nheading">
+                            <a href="news">"India's historic win at gabba: India won by 3 wickets at gabba."</a>
+                        </h3>
+                        <div class="date-update">publish on : 10 Feb 2022, 10:10 Am </div>
+                    </div>
+                    <div class="row-news">
+                        <h3 class="first-Nheading">
+                            <a href="news">"RBI keeps repo rate unchanged at 4%; maintains accommodative stance"</a>
+                        </h3>
+                        <div class="date-update">publish on : 10 Feb 2022, 10:10 Am </div>
+                    </div>
+                    <div class="row-news">
+                        <h3 class="first-Nheading">
+                            <a href="news">"Global Covid-19 case counts decline 17% in latest week:World Health
+                                Organisation(WHO)"</a>
+                        </h3>
+                        <div class="date-update">publish on : 10 Feb 2022, 10:10 Am </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </aside>
+
+
+
+    </section>
+    <section class="footer content-wrapper Footer-content">
+        <div class="f-wrapper">
+            <ul>
+                <li><a href="#About us">About us</a></li>
+                <li><a href="#Terms and Condition">Terms and Condition</a></li>
+                <li><a href="#policy">Privacy Policy</a></li>
+                <li><a href="#Publish your own Ad">Publish your Ad</a></li>
+                <li><a href="#Publish your own Ad">Advertise with us</a></li>
+                <li><a href="#Epaper">Epaper</a></li>
+                <li><a href="#Subcription">Subcription</a></li>
+                <li><a href="#Contact us">Contact us</a></li>
+            </ul>
+        </div>
+        <div class="f-wrapper follow">
+            <h3>Follow us on :</h3>
+
+            <div class="follow-icon">
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-instagram"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-linkedin"></a>
+
+
+            </div>
+        </div>
+
+    </section>
+    <footer>
+        <div class="copy-right">
+            Copyright &copy; www.TheDailyHub.com. All rights reserved!
+        </div>
+    </footer>
+    </div>
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,112 @@
+<?php
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include "common_content\db.php";
+    // if (isset($_POST['register'])) {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $Password = $_POST['Password'];
+    $cpassword = $_POST['cpass'];
+    echo $email;
+
+
+    # code...
+    $sql = mysqli_query($conn, "INSERT INTO `daily_hub_login` (`first_name`, `last_name`, `email`, `Password`, `date_time`) VALUES ('$first_name', '$last_name', '$email', '$cpassword', '2022-04-28 05:59:44.000000');");
+    // print_r("INSERT INTO `daily_hub_login` (`first_name`, `last_name`, `email`, `Password`, `date_time`) VALUES ('$first_name', '$last_name', '$email', '$cpassword', '2022-04-28 05:59:44.000000');");
+    $num_rows = mysqli_num_rows($sql);
+    if ($num_rows <= 1) {
+        echo '<script>window.location.href="html\index.php"</script>';
+        echo '<script>alert("Succefully register")</script>';
+    } else {
+
+        echo '<script>alert("user alredy exits")</script>';
+        echo '<script>window.location.href="html\index.php"</script>';
+    }
+    // }
+    if (isset($_POST['submit'])) {
+        if ($email == 'email' && $cpassword == 'cpass') {
+            $sql = mysqli_query($conn, "select * from `daily_hub_login` where email='$email' and 	Password='$cpassword'");
+            $num_rows = ($sql);
+            if ($num_rows == 1) {
+                echo '<script>window.location.href="html\index.php"</script>';
+                echo '<script>alert("Succefully login")</script>';
+            } else {
+
+                echo '<script>alert("enter valid credential")</script>';
+                echo '<script>window.location.href="html\index.php"</script>';
+            }
+        } else {
+            # code...
+            echo '<script>alert("user does not present")</script>';
+            echo '<script>window.location.href="html\index.php"</script>';
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>The Daily Hub</title>
+    <link rel="stylesheet" href="css/login.css">
+    <!---we had linked our css file----->
+</head>
+
+<body>
+    <?php require "common_content\_navbar.php"; ?>
+
+    <div id='login-form' class='login-page'>
+        <div class="form-box">
+            <div class='button-box'>
+                <div id='btn'></div>
+                <button type='button' onclick='login()' class='toggle-btn'>Log In</button>
+                <button type='button' onclick='register()' class='toggle-btn'>Register</button>
+            </div>
+            <form id='login' class='input-group-login'>
+                <input type='text' class='input-field' placeholder='Email Id' required name="email" id="email">
+                <input type='password' class='input-field' placeholder='Enter Password' required name="pass">
+                <input type='checkbox' class='check-box'><span>Remember Password</span>
+                <button type='submit' class='submit-btn' name="submit">Log in</button>
+            </form>
+            <form id='register' class='input-group-register'>
+                <input type='text' class='input-field' placeholder='First Name' required name="first_name">
+                <input type='text' class='input-field' placeholder='Last Name ' required name="last_name">
+                <input type='email' class='input-field' placeholder='Email Id' required name="email">
+                <input type='password' class='input-field' placeholder='Enter Password' required name="password">
+                <input type='password' class='input-field' placeholder='Confirm Password' required name="cpass">
+                <input type='checkbox' class='check-box'><span>I agree to the terms and conditions</span>
+                <button type='submit' class='submit-btn' name="register">Register</button>
+            </form>
+        </div>
+    </div>
+    </div>
+    <script>
+        var x = document.getElementById('login');
+        var y = document.getElementById('register');
+        var z = document.getElementById('btn');
+
+        function register() {
+            x.style.left = '-400px';
+            y.style.left = '50px';
+            z.style.left = '110px';
+        }
+
+        function login() {
+            x.style.left = '50px';
+            y.style.left = '450px';
+            z.style.left = '0px';
+        }
+    </script>
+    <script>
+        var modal = document.getElementById('login-form');
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
+</body>
+
+</html>
